73ad4c8b37c2f6974c855960a84da273e65abc97
蒼時弦也 <elct9620@frost.tw>
2017-07-08 15:07:39 +0800

    Make random reply

--- app/bot/message.rb
+++ app/bot/message.rb
@@ -2,8 +2,8 @@ include Facebook::Messenger
 
 Bot.on :message do |message|
   text = message.text
-  keyword = Keyword.find_by("? LIKE '%' || text || '%'", text)
-                   .order('length(text) DESC')
+  keyword = Keyword.where("? LIKE '%' || text || '%'", text)
+                   .order('length(text) DESC').sample
   if keyword
     message.reply(text: keyword.reply)
   else
