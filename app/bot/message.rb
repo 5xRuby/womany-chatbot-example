fccc67083a58ca86a7924e46458829e0b6752a96
蒼時弦也 <elct9620@frost.tw>
2017-07-08 14:19:44 +0800

    Make query sort by relative

--- app/bot/message.rb
+++ app/bot/message.rb
@@ -3,6 +3,7 @@ include Facebook::Messenger
 Bot.on :message do |message|
   text = message.text
   keyword = Keyword.find_by("? LIKE '%' || text || '%'", text)
+                   .order('length(text) DESC')
   if keyword
     message.reply(text: keyword.reply)
   else
