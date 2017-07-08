62adfcc6b519f7573d959f6a9ae754f10c9bde1c
蒼時弦也 <elct9620@frost.tw>
2017-07-08 13:53:37 +0800

    Add like query for chatbot

--- app/bot/message.rb
+++ app/bot/message.rb
@@ -2,7 +2,7 @@ include Facebook::Messenger
 
 Bot.on :message do |message|
   text = message.text
-  keyword = Keyword.find_by(text: text)
+  keyword = Keyword.find_by('text LIKE ?', "%#{text}%")
   if keyword
     message.reply(text: keyword.reply)
   else
