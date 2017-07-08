6dc47578e1f4190d67fbd2c6799d9cc1fc3b6e3c
蒼時弦也 <elct9620@frost.tw>
2017-07-08 14:19:01 +0800

    Revert like query

--- app/bot/message.rb
+++ app/bot/message.rb
@@ -2,7 +2,7 @@ include Facebook::Messenger
 
 Bot.on :message do |message|
   text = message.text
-  keyword = Keyword.find_by('text LIKE ?', "%#{text}%")
+  keyword = Keyword.find_by("? LIKE '%' || text || '%'", text)
   if keyword
     message.reply(text: keyword.reply)
   else
